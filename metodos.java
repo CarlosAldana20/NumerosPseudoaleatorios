1cdb031d67d0cb9e8324b8852c56476938c20912
Edgar Licona Cazares <edgarliconacaz@gmail.com>
2026-02-27 19:52:26 -0600

    micambioE

--- metodos.java
+++ metodos.java
@@ -2,5 +2,7 @@ public class metodos {
     public static void main (String[]args){
         System.out.println("hola a todos ");
         System.out.println("holaaa");
+        System.out.println("que onda bandaaaaaaa");
+        System.out.println("a ver si se guarda lo que escribo JAJAJA");
     }
 }
